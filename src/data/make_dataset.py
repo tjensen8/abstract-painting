163bb8cdd57bf1bc7041fa6cc0e822644731dab2
Taylor Jensen <taylor-jensen@outlook.com>
2021-05-23 18:43:44 -0500

    create script to make training dataset

---
+++ src/data/make_dataset.py
@@ -0,0 +1,12 @@
+"""
+This file makes the dataset to train the machine learning model from.
+"""
+
+
+import numpy as np
+import cv2
+import glob
+
+from load_config import load_config
+
+load_config()

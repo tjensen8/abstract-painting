8e7dbe352bd9211479a38b321dac0d543a818086
Taylor Jensen <taylor-jensen@outlook.com>
2021-05-23 19:30:51 -0500

    completed config loading function

--- src/data/load_config.py
+++ src/data/load_config.py
@@ -4,7 +4,11 @@ def load_config():
     with open("./src/config.yml") as f:
         try:
             config = yaml.safe_load(f)
+            print("Config Loaded Successfully")
             return config
         except Exception as e:
             print(e)
             return ""
+
+if __name__ == '__main__':
+    load_config()
